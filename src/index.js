ff3659d28e6cf763e388bb4d153cf345f6295af0
Sofia Kononovych <kononovuchsofia@gmail.com>
2022-09-11 16:49:41 +0300

    committed changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  const arr = [...document.getElementsByClassName('item')];
+  arr.map((item, index) => {
+    if (index % 2 === 0) {
+      return (item.style.backgroundColor = 'red');
+    }
+  });
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  let item = document.querySelector('li');
+  for (let i = 0; i < document.querySelectorAll('li').length; i++) {
+    if (item.classList.contains('likedItem')) {
+      item.style.backgroundColor = 'blue';
+    } else {
+      item = item.nextElementSibling;
+    }
+  }
+};
